150ad5f24fdce187fc6b5eb0b7592b0a76876594
Indonesia-Sadboy-Xploit <87993609+Indonesia-Sadboy-Xploit@users.noreply.github.com>
2021-07-28 16:15:16 +0700

    Create Ujicoba.sh

---
+++ Ujicoba.sh
@@ -0,0 +1,36 @@
+figlet "Ujicoba"
+echo "BY INDONESIA SADBOY XPLOIT"
+echo "LEADER I.S.X:M4SKGANS"
+echo "Co leader1:XCRAZYGANS"
+echo "Co leader2:BIHACK"
+echo "------------------------"
+echo "IG:@indonesia.sadboy.xploit"
+echo "WhatsApp:0895336062247"
+echo "TOOLS I.S.X: http://tools-isx.atwebpages.com/"
+echo "BLOGGER I.S.X: https://indonesian-sadboy-xploit.blogspot.com/"
+echo "GITHUB I.S.X: https://github.com/Indonesia-Sadboy-Xploit/"
+echo "------------------------"
+echo "1.pengertian tentang c++"
+echo "2.sejarah tentang c++"
+echo "3.pengertian tentang web crawler"
+echo "4.pengertian bitcoin"
+echo "5.exit"
+read -p "SILAHKAN PILIH!" pil
+if [ $pill = '1' ]:
+then
+echo "1.Bahasa Pemrograman C++ adalah bahasa pemrograman komputer yang bisa dipakai untuk membuat berbagai aplikasi (general-purpose programming language), dan merupakan pengembangan dari bahasa pemrograman C."
+elif [ $pil = '2' ]:
+  then
+echo "2.Bahasa pemrograman C++ dirintis sejak tahun 1979 oleh Bjarne Stroustrup, seorang ilmuwan komputer asal Denmark. Pengembangan bahasa C++ terinspirasi dari penelitian tesis Phd yang dulunya pernah dikerjakan Stroustrup."
+elif [ $pil = '3' ]:
+  then
+echo "3.Web Crawler adalah proses mencari tau kumpulan halaman dari sebuah web untuk dilakukan pengindeksan."
+elif [ $pil = '4' ]:
+  then
+echo "4.Bitcoin adalah salah satu jenis mata uang digital yang diciptakan pada Januari 2009, tepatnya saat jatuhnya pasar perumahan. Penjelasan tentang apa itu Bitcoin tentunya belum lengkap tanpa menyebutkan nama penemunya, yaitu Satoshi Nakamoto, yang merupakan sebuah nama samaran. Hingga kini identitas orang yang menciptakan Bitcoin ini adalah salah satu misteri yang masih belum terpecahkan."
+elif [ $pil = '5' ]:
+  then
+echo "TERIMAKASIH TELAH MAMPIR:V"
+else
+echo "gak ada bangsat:V"
+fi
