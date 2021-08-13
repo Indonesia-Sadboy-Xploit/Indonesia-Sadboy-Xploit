799daf89708312f7a3a21aefa273a00d9566b398
Indonesia-Sadboy-Xploit <87993609+Indonesia-Sadboy-Xploit@users.noreply.github.com>
2021-08-13 09:00:26 +0700

    Sc-By-I.S.X

---
+++ Sc-By-I.S.X.sh
@@ -0,0 +1,36 @@
+default="\033[00m"
+hijau="\033[32m"
+
+figlet "TUTORIAL DAN CARA HACKING"
+echo $hijau"SC BY INDONESIA SADBOY XPLOIT"
+echo "---------------------------------------"
+echo "LEADER:M4SKGANS"
+echo "CO LEADER 1:XCRAZYGANS"
+echo "CO LEADER 2:BIHACKGANS"
+echo "---------------------------------------"
+echo "WHATSAPP:0895336062247"
+echo "INSTAGRAM:@indonesia_sadboy_xploit"
+echo "FACEBOOK: INDONESIA SADBOY XPLOIT"
+echo "Blogger:indonesian-sadboy-xploit.blogspot.com"
+echo "Tools: http:Tools-indonesia-sadboy-xploit.xp3.biz/"
+echo "---------------------------------------"
+echo "1.pengertian dari Ransomware"
+echo "2.pengertian dari Deface"
+echo "3.pengertian dari phising"
+echo "4.exit"
+read -p "PILIH AJA TOD:V" pil
+if [ $pil = '1' ]:
+then
+echo "1.Apa Itu Ransomware?? Ransomware adalah salah satu jenis Malware yang digunakan peretas guna mengenkripsi data korban kemudian meminta uang tebusan untuk memulihkannya."
+elif [ $pil = '2' ]:
+  then
+echo "2.Apa Itu Deface?? Deface atau Defacing adalah sebuah kegiatan yang dilakukan untuk mengubah tampilan pada suatu website dengan cara menyisipkan file pada server website tersebut. Teknik ini bisa dilakukan karena adanya celah keamanan yang ada pada sebuah website."
+elif [ $pil = '3' ]:
+  then
+echo "3.Apa Itu Phising?? Dalam komputer, phising(pengelabuan) adalah suatu bentuk penipuan yang dicirikan dengan percobaan untuk mendapatkan informasi peka, seperti kata sandi dan kartu kredit, dengan menyamar sebagai orang atau bisnis yang tepercaya dalam sebuah komunikasi elektronik resmi, seperti surat elektronik atau pesan instan."
+elif [ $pil = '4' ]:
+  then
+echo "TERIMAKASIH TELAH MAMPIR:V"
+else
+echo "gak ada bangsat:V"
+fi
